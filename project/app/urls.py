8872f72c1315baf87612277a02a444dd9f2a0e4c
manikanta <mmaturu@vivifyhealthcare.com>
2023-08-23 08:55:43 +0530

    Latest code

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('CreateNoteTablePost/', CreateNoteTablePost.as_view(), name='CreateNoteTable'),
-    path('InsertDataintoDynamoDB/', InsertDataintoDynamoDB.as_view(), name='InsertDataintoDynamoDB'),
+    path('CreateNotesTable/', CreateNoteTablePost.as_view(), name='CreateNoteTable'),
+    path('CreateNotes/', InsertDataintoDynamoDB.as_view(), name='InsertDataintoDynamoDB'),
 
 ]
 
